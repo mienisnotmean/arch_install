56d25660df5c1b7d1f7ff973a2e2b1120b4a30dd
Mien <mienth2@gmail.com>
2020-06-29 22:04:06 +0200

    add fonts

---
+++ 103-fonts.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+func_install() {
+    if pacman -Qi $1 &> /dev/null; then
+	tput setaf 2
+  	echo "###############################################################"
+  	echo "# The package " $1 " is already installed"
+      	echo "###############################################################"
+      	echo
+	tput sgr0
+    else
+    	tput setaf 3
+    	echo "###############################################################"
+    	echo "# Installing package " $1
+    	echo "###############################################################"
+    	echo
+    	tput sgr0
+    	sudo pacman -S --noconfirm --needed $1
+    fi
+}
+
+package_list=(
+    noto-fonts
+    noto-fonts-cfk
+    noto-fonts-emoji
+    noto-fonts-extra
+    awesome-terminal-fonts
+    ttf-font-awesome
+    ttf-hack
+    ttf-roboto
+    ttf-dejavu
+    ttf-droid
+)
+
+count=0
+
+for name in "${package_list[@]}" ; do
+    count=$[count+1]
+    tput setaf 3;echo "Installing package nr. " $count " " $name;tput sgr0;
+    func_install $name
+done
+
+tput setaf 11;
+echo "################################################################"
+echo "# Fonts have been installed"
+echo "################################################################"
+echo;tput sgr0
