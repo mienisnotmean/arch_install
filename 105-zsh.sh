c31d05d0478a327eb78dce8994ecf5440c04a9b6
Mien <mienth2@gmail.com>
2020-07-01 22:14:44 +0200

    update zsh

--- 105-zsh.sh
+++ 105-zsh.sh
@@ -11,3 +11,7 @@ sudo -u mien git clone --depth=1 https://github.com/zsh-users/zsh-completions.gi
 sudo -u mien git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git /home/mien/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 cp /home/mien/.oh-my-zsh/templates/zshrc.zsh-template /home/mien/.zshrc
+
+git clone https://github.com/dracula/gnome-terminal /home/mien/gnome-terminal
+sudo -u mien /home/mien/gnome-terminal/install.sh
+rm -r /home/mien/gnome-terminal
