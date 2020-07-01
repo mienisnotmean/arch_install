23c4bc7e7827ec1673eafcac305b5fd7a325e952
Mien <mienth2@gmail.com>
2020-07-01 21:49:04 +0200

    update zsh

--- 105-zsh.sh
+++ 105-zsh.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-git clone https://github.com/ohmyzsh/ohmyzsh.git /home/mien/.oh-my-zsh
+sudo -u mien git clone https://github.com/ohmyzsh/ohmyzsh.git /home/mien/.oh-my-zsh
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/mien/.oh-my-zsh/custom/themes/powerlevel10k
+sudo -u mien git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/mien/.oh-my-zsh/custom/themes/powerlevel10k
 
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git /home/mien/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+sudo -u mien git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git /home/mien/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-git clone --depth=1 https://github.com/zsh-users/zsh-completions.git /home/mien/.oh-my-zsh/custom/plugins/zsh-completions
+sudo -u mien git clone --depth=1 https://github.com/zsh-users/zsh-completions.git /home/mien/.oh-my-zsh/custom/plugins/zsh-completions
 
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git /home/mien/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+sudo -u mien git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git /home/mien/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 cp /home/mien/.oh-my-zsh/templates/zshrc.zsh-template /home/mien/.zshrc
