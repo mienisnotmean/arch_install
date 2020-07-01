398e9411b36f923a0b317e9e825cd0dcdb82f845
Mien <mienth2@gmail.com>
2020-07-01 18:17:55 +0200

    update zsh

--- 105-zsh.sh
+++ 105-zsh.sh
@@ -2,12 +2,12 @@
 
 git clone https://github.com/ohmyzsh/ohmyzsh.git /home/mien/.oh-my-zsh
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-/home/mien/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/mien/.oh-my-zsh/custom/themes/powerlevel10k
 
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-/home/mien/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git /home/mien/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-git clone --depth=1 https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-/home/mien/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone --depth=1 https://github.com/zsh-users/zsh-completions.git /home/mien/.oh-my-zsh/custom/plugins/zsh-completions
 
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-/home/mien/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git /home/mien/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 cp /home/mien/.oh-my-zsh/templates/zshrc.zsh-template /home/mien/.zshrc
