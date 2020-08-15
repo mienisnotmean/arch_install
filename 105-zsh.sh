8a6ad021a4e45254512e072b0c7b958b82319787
Mien <mienth2@gmail.com>
2020-08-15 22:12:51 +0200

    update zsh

--- 105-zsh.sh
+++ 105-zsh.sh
@@ -1,40 +1,14 @@
 #!/bin/bash
 
-func_install() {
-    if pacman -Qi $1 &> /dev/null; then
-	tput setaf 2
-  	echo "###############################################################"
-  	echo "# The package " $1 " is already installed"
-      	echo "###############################################################"
-      	echo
-	tput sgr0
-    else
-    	tput setaf 3
-    	echo "###############################################################"
-    	echo "# Installing package " $1
-    	echo "###############################################################"
-    	echo
-    	tput sgr0
-    	sudo pacman -S --noconfirm --needed $1
-    fi
-}
-
-package_list=(
-    zsh-autosuggestion
-    zsh-completions
-    zsh-syntax-highlighting
-    zsh-theme-powerlevel10k
-)
-
-count=0
-
 sudo -u mien git clone https://github.com/ohmyzsh/ohmyzsh.git /home/mien/.oh-my-zsh
 
-for name in "${package_list[@]}" ; do
-    count=$[count+1]
-    tput setaf 3;echo "Installing package nr. " $count " " $name;tput sgr0;
-    func_install $name
-done
+sudo -u mien git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/mien/.oh-my-zsh/custom/themes/powerlevel10k
+
+sudo -u mien git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git /home/mien/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+sudo -u mien git clone --depth=1 https://github.com/zsh-users/zsh-completions.git /home/mien/.oh-my-zsh/custom/plugins/zsh-completions
+
+sudo -u mien git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git /home/mien/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 sudo -u mien cp arch_install/dotfiles/.zshrc /home/mien/.zshrc
 
