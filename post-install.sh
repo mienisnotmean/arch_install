aa5692999ad74aa39f6b189a53d645634df442f3
mien <mienth2@gmail.com>
2020-08-18 18:27:37 +0200

    update post install

--- post-install.sh
+++ post-install.sh
@@ -7,7 +7,7 @@ git clone https://aur.archlinux.org/trizen.git
 (cd trizen && makepkg -si)
 rm -rf trizen
 
-trizen -S ant-dracula-gtk-theme
+trizen -S ant-dracula-gtk-theme --noconfirm
 
 git clone https://github.com/dracula/gnome-terminal
 gnome-terminal/install.sh
