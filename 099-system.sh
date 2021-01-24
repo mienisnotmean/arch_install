6ad1ea4a2d61797570867c806cd31913c9c4da3e
mien <mienth2@gmail.com>
2021-01-24 16:50:58 +0100

    update system

--- 099-system.sh
+++ 099-system.sh
@@ -20,10 +20,20 @@ grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchL
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Set root password
+tput setaf 11;
+echo "################################################################"
+echo "# Enter root password:"
+echo "################################################################"
+echo;tput sgr0
 passwd
 
 # Create user
 useradd -m -g users -s $(which zsh) mien
 gpasswd -a mien wheel
+tput setaf 11;
+echo "################################################################"
+echo "# Enter root password:"
+echo "################################################################"
+echo;tput sgr0
 passwd mien
 sed -i '/%wheel ALL=(ALL) ALL/s/^# //g' /etc/sudoers
